d502c27b1793a92230935069a5c576698f4b2a6a
Laís Urano <lais.urano@gmail.com>
2024-07-02 15:38:46 -0300

    teste de tempo

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import anthropic
 import dotenv 
 import os
+import time
+
+# Marca o início do tempo
+start_time = time.time()
 
 dotenv.load_dotenv()
 client = anthropic.Anthropic(
@@ -9,17 +13,20 @@ client = anthropic.Anthropic(
 )
 
 message = client.messages.create(
-    model="claude-3-opus-20240229",
+    model="claude-3-opus-20240229",# 4.545861721038818 segundos | 2.9456045627593994 segundos
+    # model="claude-3-5-sonnet-20240620",#1.962998628616333 segundos | 1.775184154510498 segundos
+    # model= "claude-3-sonnet-20240229", #4.803567171096802 segundos | 3.022520065307617 segundos
+    # model="claude-3-haiku-20240307",#2.4997713565826416 segundos | 1.327709436416626 segundos
     max_tokens=1000,
     temperature=0,
-    system="Listar apenas os nomes dos produtos, sem considerar descrição.",
+    system="Listar apenas os nomes dos alimentos, sem considerar descrição.",
     messages=[
         {
             "role": "user",
             "content": [
                 {
                     "type": "text",
-                    "text": "liste 3 produtos sustentáveis\n"
+                    "text": "liste 3 alimentos veganos\n"
                 }
             ]
         }
@@ -32,3 +39,11 @@ conteudo_texto = message.content[0].text #posição 0 da lista de text
 
 # Exibindo o texto
 print(conteudo_texto)
+
+# Marca o fim do tempo
+end_time = time.time()
+
+# Calcula o tempo de execução
+execution_time = end_time - start_time
+
+print(f"Tempo de execução: {execution_time} segundos")
