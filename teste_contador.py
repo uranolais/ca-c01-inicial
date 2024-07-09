cec220e42b3a063cbc2b7b8a630360407e95f7fe
Laís Urano <lais.urano@gmail.com>
2024-07-09 15:35:56 -0300

    alterando contador

--- teste_contador.py
+++ teste_contador.py
@@ -4,27 +4,32 @@ import dotenv
 
 dotenv.load_dotenv()
 api_key = os.environ.get("ANTHROPIC_API_KEY")
-headers = {
-    "x-api-key": api_key,
-    "anthropic-version": "2023-06-01",
-    "content-type": "application/json"
-}
-
-response = requests.post(
-    "https://api.anthropic.com/v1/messages",
-    headers=headers,
-    json={
-        "model": "claude-3-5-sonnet-20240620",
-        "max_tokens": 1024,
-        "messages": [{"role": "user", "content": "Hello, Claude"}]
+modelo = "claude-3-5-sonnet-20240620"
+def conta_tokens(modelo):
+    headers = {
+        "x-api-key": api_key,
+        "anthropic-version": "2023-06-01",
+        "content-type": "application/json"
     }
-)
+    response = requests.post(
+        "https://api.anthropic.com/v1/messages",
+        headers=headers,
+        json={
+            "model": modelo,
+            "max_tokens": 1024,
+            "messages": [{"role": "user", "content": "Hello, Claude"}]
+        }
+    )
 
-# Acessar os cabeçalhos relevantes
-tokens_limit = response.headers.get('anthropic-ratelimit-tokens-limit')
-tokens_remaining = response.headers.get('anthropic-ratelimit-tokens-remaining')
-tokens_reset = response.headers.get('anthropic-ratelimit-tokens-reset')
+    # Acessar os cabeçalhos relevantes
+    tokens_limit = response.headers.get('anthropic-ratelimit-tokens-limit')
+    tokens_remaining = response.headers.get('anthropic-ratelimit-tokens-remaining')
+    tokens_reset = response.headers.get('anthropic-ratelimit-tokens-reset')
 
-print(f"Limite de tokens: {tokens_limit}")
-print(f"Tokens restantes: {tokens_remaining}")
-print(f"Reset do limite: {tokens_reset}")
+    # print(f"Limite de tokens: {tokens_limit}")
+    # print(f"Tokens restantes: {tokens_remaining}")
+    # print(f"Reset do limite: {tokens_reset}")
+
+    return tokens_limit
+
+conta_tokens(modelo=modelo)
